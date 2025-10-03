eb33340c186ee9e7c5c4e038a17af0df8b45aec9
Massil <160241880+Massil-br@users.noreply.github.com>
2025-10-03 11:56:12 +0200

    put/delete/post/get of individual users

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -19,8 +19,42 @@ export async function AddUser(req:Request,res:Response){
     if(userExist){
         return res.status(401).json("Conflict can't create user");
     }
-    
     users.push(username);
     return res.status(201).json("User created");
 
+}
+
+export async function GetUserById(req:Request,res:Response){
+    const id = parseInt(req.params.id);
+
+    const user = users[id];
+    if(!user){
+        return res.status(404).json("user not found");
+    }
+    res.status(200).json({user})
+}
+
+export async function EditUser(req:Request,res:Response){
+    const newUser = req.body.username;
+    const id = parseInt(req.params.id);
+    const user = users[id];
+    if (!user){
+        return res.status(404).json("user not found")
+    }
+    if (!newUser || newUser == ""){
+        return res.status(401).json("username can't be empty")
+    }
+    users[id] = newUser
+    return res.status(201).json({newUser});
+}
+
+export async function DeleteUser(req:Request,res:Response){
+    const id = parseInt(req.params.id);
+    const user = users[id];
+    if (!user){
+        return res.status(404).json("user not found");
+    }
+
+    users.splice(id,1);
+    return res.status(201).json(`User ${user} deleted successfully`);
 }
--- src/Routes/User.ts
+++ src/Routes/User.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { GetAllUsers,AddUser } from "../Controllers/userController.js";
+import { GetAllUsers,AddUser, GetUserById, EditUser, DeleteUser } from "../Controllers/userController.js";
 
 
 
@@ -7,9 +7,11 @@ import { GetAllUsers,AddUser } from "../Controllers/userController.js";
 const router = express.Router();
 
 
-router.get("/",GetAllUsers)
-router.post("/",AddUser)
-
+router.get("/",GetAllUsers);
+router.post("/",AddUser);
+router.get("/:id",GetUserById);
+router.put("/:id",EditUser);
+router.delete("/:id",DeleteUser)
 
 
 export default router;
